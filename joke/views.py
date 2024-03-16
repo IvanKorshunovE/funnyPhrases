92b95195d9c9ec1234a0eb269e29e011e7ed7b89
Ivan <ivan.korshunov.work@gmail.com>
2024-03-16 21:25:09 +0200

    commit

--- joke/views.py
+++ joke/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from joke.models import TestModel
 
 
 class TestView(APIView):
     def get(self, request):
-        return Response("Hello There (2024 Mar 14:26)")
+        users = TestModel.objects.values("text")
+        user_dict = {user["text"]: user for user in users}
+        return Response(f"Hello There (2024 Mar 14:26), {user_dict}")
